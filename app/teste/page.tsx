1a427facf9168b0d8b06da4c2d42b2a492e8c74d
WalliFinance <lopesluan431@gmail.com>
2024-07-15 11:52:06 -0300

    Test repo

---
+++ app/teste/page.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from 'next'
+
+interface Props {}
+
+const Page: NextPage<Props> = ({}) => {
+  return (
+    <div>
+    <p>Teste</p>
+    <p>teste 2</p>
+    </div>
+  )
+}
+
+export default Page
